d1aa9fd05efb259944b943cd6595e2d3b7ac60aa
LGL <liguangliang@lisinuo.net>
2023-07-10 18:01:48 +0800

    base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/MyBlog/',
   title: "LGL",
   description: "A VitePress Site",
   themeConfig: {
